a692c9869b0e13a4181a8faf75b50986952d1fb6
alucascs <25238141-alucascs@users.noreply.replit.com>
2023-08-09 21:35:44 +0000

    dasdsap

--- .ccls-cache/@home@runner@inf029-AndreLucas/ProjetoEscola@main.c
+++ .ccls-cache/@home@runner@inf029-AndreLucas/ProjetoEscola@main.c
@@ -1,25 +1,5 @@
 #include <stdio.h>
 
 int main(void){
-  int idade[10], i;
-  float media;
-
-  media = 0;
-  
-  for(i=0;i<10;i++){
-    printf("digite a idade numero %d: ", i+1);
-    scanf("%d", &idade[i]);
-    media += idade[i];
-  }
-  
-  media = media/10;
-
-  printf("A media das idades e igual a %.2f\n", media);
-  printf("As idades que sao maiores que a media sao:\n");
-  
-  for(i=0;i<10;i++){
-    if(idade[i]>media){
-      printf("%d\n", idade[i]);
-    }
-  }
+  printf("teste");
 }
